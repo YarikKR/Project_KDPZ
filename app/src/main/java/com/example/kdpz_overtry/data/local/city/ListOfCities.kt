14447a851339aec47c664fc85936c9dac9ca3362
YarikKR <yvolk2018@gmail,com>
2023-05-31 22:30:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kdpz_overtry/data/local/city/ListOfCities.kt
@@ -0,0 +1,15 @@
+package com.example.kdpz_overtry.data.local.city
+
+object ListOfCities {
+    val listCity = listOf(
+        City("Київ","https://lexinform.com.ua/wp-content/uploads/2021/05/aerial-view-to-the-motherland-statue-in-the-kiev-w-FAYR9MX-scaled.jpg" ),
+        City("Житомир", "https://upload.wikimedia.org/wikipedia/commons/6/68/%D0%91%D1%83%D0%B4%D0%B8%D0%BD%D0%BE%D0%BA_%D0%B2%D0%B7%D0%B0%D1%94%D0%BC%D0%BD%D0%BE%D0%B3%D0%BE_%D0%BA%D1%80%D0%B5%D0%B4%D0%B8%D1%82%D1%83_%28%D0%BC%D1%96%D1%81%D1%8C%D0%BA%D0%B0_%D1%80%D0%B0%D0%B4%D0%B0%29_.jpg"),
+        City("Львів","https://prolviv.com/wp-content/uploads/2023/05/lviv_sunrises_and_sunsets.jpg"),
+        City("Харків","https://f.discover.ua/city/30/HBVfD.jpg"),
+        City("Одеса","https://24tv.ua/resources/photos/news/202107/1685585.jpg?v=1661261418000"),
+        City("Тернопіль","https://ternopilcity.gov.ua/upload/DJI-0006-2.jpg"),
+        City("Ружин","https://travels.in.ua/api/Photo/PhotoStreamCIL/6465"),
+        City("Ніжин","https://7chudes.in.ua/wp-content/uploads/2016/05/MG_2579.jpg"),
+        City("Чернігів","https://rsidehotel.com/wp-content/uploads/2018/12/chern3.jpg")
+    )
+}
